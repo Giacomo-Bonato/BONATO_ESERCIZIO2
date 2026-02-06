b669c0cac4eef101fb15eccf64afc984242a0ce0
Giacomo Bonato <Giacomo@msi.com>
2026-02-06 19:11:27 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> v = {5, 18, 21, 40, 7, 9, 60, 11, 3, 100, 8, 2};
+
+    auto maggiore = std::find_if(v.begin(), v.end(), [](int i) {
+    return i > 58;
+    });
+    std::cout << *maggiore << std::endl;
+
+    v.erase(
+    std::remove_if(v.begin(), v.end(), [](int i) {
+        return i%2 == 1;
+    }),
+    v.end()
+    );
+
+    for (int i : v) {
+        std::cout << i << " ";
+    }
+
+    return 0;
+}
